b3cf3bb83e169da3b79f48bff3c16d83c0dffba1
Varun Verma <varunverma855@gmail.com>
2022-11-26 05:09:03 -0500

    Flash loan complete

--- test/Lesson 3 Test - FeiRari.t.sol
+++ test/Lesson 3 Test - FeiRari.t.sol
@@ -7,31 +7,41 @@ import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 contract FeiRariExploit is Test {
     BalancerInterface public balancerFlashLoanContract;
+    WethInterface public weth;
 
     function setUp() public {
         vm.startPrank(address(0x7));
         vm.rollFork(14684371);
 
         balancerFlashLoanContract = BalancerInterface(payable(0xBA12222222228d8Ba445958a75a0704d566BF2C8));
+        weth = WethInterface(payable(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
     }
 
     function testExploitRari() public {
         // console.log("hey");
-        address[] memory path = new address[](1);
-        path[0] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        uint256[] memory amounts = new uint256[](1);
-        amounts[0] = 1;
-        bytes memory userData = "";
 
-        balancerFlashLoanContract.flashLoan(address(0x7), path, amounts, userData);
+        vm.deal(address(0x7), 10000 ether);
+        weth.deposit{value: 10000 ether}();
+        weth.approve(address(this), 1000 ether);
+
+
+        address[] memory path = new address[](1);
+        path[0] = address(weth);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1 ether;
+        bytes memory userData = "";
+        console.log("hey!!!", address(this));
+        balancerFlashLoanContract.flashLoan(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496, path, amounts, userData);
     }
 
-    // function receiveFlashLoan(
-    //     IERC20[] memory tokens,
-    //     uint256[] memory amounts,
-    //     uint256[] memory feeAmounts,
-    //     bytes memory userData
-    // ) external override {
-    //     require(msg.sender == vault);
-    // }
+    function receiveFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) external  {
+        // require(msg.sender == 0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+        console.log("Received!");
+        weth.transferFrom(address(0x7), msg.sender, 2 ether);
+    }
 }
